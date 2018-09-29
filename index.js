174659a6c315a1b6cfb337d4f2572cabc23b4012
ij2872 <ijaimes2872@gmail.com>
2018-09-29 12:06:29 -0500

    added global.css

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-js
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-override");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
