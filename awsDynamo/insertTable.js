8ce60b0be2eb48c29a9f957c0330c552901b8ddf
stavo41 <jimenez.gustavo41@gmail.com>
2018-12-28 23:24:52 -0700

    how to add an item to DB

---
+++ awsDynamo/insertTable.js
@@ -0,0 +1,40 @@
+//create a new item
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    credentials: {
+        accessKeyId: 'omitted',
+        secretAccessKey: 'omitted'
+    },
+    region: "us-east-2",
+    endpoint: "https://dynamodb.us-east-2.amazonaws.com"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "utashpe";
+
+var email = 'john.doe@mavs.uta.edu';
+var fName = 'John';
+var lName = 'doe';
+var major = 'Mechanical Engineering';
+
+var params = {
+    TableName: table,
+    Item:{
+        "email": email,
+        "FName": fName,
+        "LName": lName,
+        'Major': major
+        
+    }
+};
+
+console.log("Adding a new item...");
+docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
